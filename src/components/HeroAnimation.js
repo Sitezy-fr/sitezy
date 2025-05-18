e98aa66ecbd31e4824cd1716e5684a54dbe037ec
Rémi Hopsore <remi@macbookair.home>
2025-05-18 02:36:55 +0200

    fix Hero

--- src/components/HeroAnimation.js
+++ src/components/HeroAnimation.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
-import animationData from '../assets/Hero-opti.json'; // Chemin vers ton fichier JSON
-
+import animationData from '../assets/Hero.json';
 
 const HeroAnimation = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Vérifie si l’écran est de type mobile (largeur max 768px)
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div>
-      <Lottie className='hero-container'
-        animationData={animationData} // Ici, tu utilises l'attribut `animationData` au lieu de `src`
-        loop={true}  // Définit si l'animation doit boucler (true ou false)
+      <Lottie
+        className="hero-container"
+        animationData={animationData}
+        loop={!isMobile} // ✅ Désactive la boucle sur mobile
       />
     </div>
   );
